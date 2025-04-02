2294d3cb1af52e9967b1f6a5b4e345e42d8e7bfa
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2025-04-02 14:42:16 +0900

    cleanup phase .1

--- 0A_ascii_game_tuts/marrokrpg.rb
+++ 0A_ascii_game_tuts/marrokrpg.rb
@@ -1,22 +1,48 @@
+require 'json'
+require 'io/console'
+
 run = true
 menu = true
 play = false
 rules = false
 
-def save(name)
+hp = 50
+attack = 6
+name = ""
+
+def clear()
+  system("cls") || system("clear")
+end
+
+def save(name, hp, attack)
   data = {
     name: name,
+    hp: hp,
+    attack: attack 
   }
-  File.open("load.txt","w") { |file| file.write(data) }
+  File.open("load.json","w") { |file| file.write(data.to_json) }
+end
+
+def load()
+  file_content = File.read("load.json") 
+  data = JSON.parse(file_content)
+
+  name = data['name']
+  hp = data['hp']
+  attack = data['attack']
+
+  puts "This data is loaded #{name} #{hp} #{attack}"
 end
 
 while run
 
   while menu
-    
+   
+    clear()
+
     puts "-------------"
     puts "1 NEW GAME "
-    puts "2 SAVE GAME "
+    puts "2 LOAD GAME "
     puts "3 RULES "
     puts "4 QUIT GAME "
     puts "-------------"
@@ -29,16 +55,16 @@ while run
       menu = false
       play = true
     elsif choice == "2"
-      if name == ""
-        puts "You have not created a character yet"
-      end
-
-      save(name)
-
+      load()
+      puts "Game Loaded"
+      puts "Your name is #{name}"
+      puts "Press any key..."
+      STDIN.gets
     elsif choice == "3"
       puts "These are the rules"
-      # choice = ""
-      # break
+      puts "Your name is #{name}"
+      puts "Press any key..."
+      STDIN.gets
     elsif choice == "4"
       puts "Exiting Tales of Marrok"
       exit
@@ -47,6 +73,9 @@ while run
   end
 
   while play
+
+    save(name, hp, attack) #autosave
+
     puts "Welcome to the game #{name}"
     dest = gets.chomp
 
